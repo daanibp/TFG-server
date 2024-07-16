26df02cc1b8d9b9fa6ec43e55e125211a89c1452
daniel <uo277476@uniovi.es>
2024-07-16 18:45:44 +0200

    Cambios cors prueba

--- index.js
+++ index.js
@@ -4,7 +4,27 @@ const cors = require("cors");
 require("dotenv").config();
 
 app.use(express.json());
-app.use(cors());
+//app.use(cors());
+
+const allowedOrigins = [
+    "https://https://blue-moss-0c93d7110.5.azurestaticapps.net/",
+];
+
+app.use(
+    cors({
+        origin: function (origin, callback) {
+            if (!origin) return callback(null, true);
+            if (allowedOrigins.indexOf(origin) === -1) {
+                const msg =
+                    "El CORS policy no permite el acceso desde el origen especificado.";
+                return callback(new Error(msg), false);
+            }
+            return callback(null, true);
+        },
+        methods: "GET,POST,PUT,DELETE",
+        allowedHeaders: "Content-Type,Authorization",
+    })
+);
 
 const db = require("./models");
 
